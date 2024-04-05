4c6128e38818da70bc129bdf23eddea400f36880
amorimzyx <160196909+amorimsilv@users.noreply.github.com>
2024-04-05 18:26:10 -0300

    Repetições em JS

---
+++ 13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6) /* Uso do "DO"*/
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1                     Uso do WHILE, todos tendo a mesma funcionalidade
+}
+*/
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
