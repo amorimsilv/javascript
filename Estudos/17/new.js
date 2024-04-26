f6d5a7b9d0fdd38026460a1e5ddffb787c33d83d
amorimzyx <160196909+amorimsilv@users.noreply.github.com>
2024-04-25 21:06:48 -0300

    new.js

---
+++ Estudos/17/new.js
@@ -0,0 +1,37 @@
+/*
+
+SISTEMA DE SABER SE É JOVEM OU NÃO
+
+var idade = 16
+if (idade <= 17) {
+    console.log(`Você tem ${idade} anos. Você ainda é um Jovem!!`)
+} else {
+    console.log(`Você tem ${idade} anos. Você agora é um Adulto!!`)
+}
+
+*/
+
+
+
+// SISTEMA DE CNH / SISTEMA DE CNH / SISTEMA DE CNH / SISTEMA DE CNH / SISTEMA DE CNH / SISTEMA DE CNH /  
+/*
+var idade = 18
+
+if (idade == 0) {
+    console.log(`[ERROR] Reconhecemos que não é possivel que você tenha ${idade} ano de idade :(`)
+} else if (idade <= 1) {
+    console.log(`Você tem ${idade} ano de idade!`)
+} else if (idade >= 2) {
+    console.log(`Você tem ${idade} anos de idade!`)
+}
+
+if (idade == 0){
+    console.log(``)
+} else if (idade <= 17) {
+    console.log(`Você ainda não pode tirar a sua CNH (A e B), somente com 18 anos de idade! :(`)
+} else if (idade >= 18) {
+    console.log(`Você já pode tirar a sua CNH (A e B)! :)`)
+}
+*/
+
+
