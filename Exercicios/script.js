460af2f6d0fc6e8251f13c23652cf83cffa3ced9
amorimzyx <160196909+amorimsilv@users.noreply.github.com>
2024-04-28 14:20:13 -0300

    ..

---
+++ Exercicios/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'imagens/dia.jpeg'
+        document.body.style.background = '#3370A6'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'imagens/tarde.jpeg'
+        document.body.style.background = '#1B3240'
+    } else {
+        // BOA NOITE!
+        img.src = 'imagens/noite.jpeg'
+        document.body.style.background = '#012626'
+    }
+}
+
