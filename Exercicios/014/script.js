b845607891498cc36437418517696ab5433aaf2b
amorimzyx <160196909+amorimsilv@users.noreply.github.com>
2024-04-28 23:17:13 -0300

    33333

--- Exercicios/014/script.js
+++ Exercicios/014/script.js
@@ -2,9 +2,10 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 12
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 0 && hora <= 12) {
         // BOM DIA!
         img.src = 'imagens/dia.jpeg'
         document.body.style.background = '#3370A6'
