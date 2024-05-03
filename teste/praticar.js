173a3efa1215c74da4fac61ce9f98d411b9ab2b6
amorimzyx <160196909+amorimsilv@users.noreply.github.com>
2024-05-03 20:43:20 -0300

    Create praticar.js

---
+++ teste/praticar.js
@@ -0,0 +1,7 @@
+var dinheiro = 25
+console.log(`Você possue ${dinheiro} reais em sua conta bancaria!`)
+if (dinheiro <= 25) {
+    console.log(`Você pode está indo a beira da falência, ligue para o nosso banco e peça já um empréstimo!`)
+} else if (dinheiro >= 50.000) {
+    console.log('Caso queira fazer uma doação para algum tipo de instituição, como por exemplo a AACD, que garante a assistência médico-terapêutica de excelência em Ortopedia e Reabilitação, entre em contato conosco!')
+}
