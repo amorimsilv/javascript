ee896798d62754c8957a0d4eb6653ca91367c595
amorimzyx <160196909+amorimsilv@users.noreply.github.com>
2024-05-06 22:31:13 -0300

    Callbacks, funções anônimas

---
+++ Estudos avançados/funções/funções anônimas/script.js
@@ -0,0 +1,7 @@
+// https://www.youtube.com/watch?v=2EAfSeKZNiQ
+
+const botao = document.getElementById('botao-funcao')
+
+botao.addEventListener('click', function() {
+    alert('Você clicou no botão!')
+})
