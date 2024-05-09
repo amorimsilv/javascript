12d18aa765a653ed5f7af2e59a5bb2f1e81d055f
amorimzyx <160196909+amorimsilv@users.noreply.github.com>
2024-05-09 20:01:08 -0300

    33

--- Estudos avançados/funções/funções anônimas/script.js
+++ Estudos avançados/funções/funções anônimas/script.js
@@ -5,3 +5,13 @@ const botao = document.getElementById('botao-funcao')
 botao.addEventListener('click', function() {
     alert('Você clicou no botão!')
 })
+
+const botao2 = document.getElementById('botaotwo')
+
+botao2.addEventListener('click', function() {
+    botao2.style.opacity = 0
+})
+
+botao2.addEventListener('onclick', function() {
+    botao2.style.opacity = 0
+})
