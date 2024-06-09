b4e9089ae3ddf67c2355666ba40a9cf8f67e0f9c
amorim <amorimff35@gmail.com>
2024-06-09 13:30:16 -0300

    1

---
+++ Estudos avançados/Curso JavaScript Matheus Battisti/6 - String/js/scripts.js
@@ -0,0 +1,16 @@
+var nome = "Matheus";
+var sobrenome = "Amorim";
+var numero = 532;
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto)
+
+document.write("Olá!");
+
+document.write('Ele disse: "Olá!"');
+
+console.log("Este é o nome do usuario: " + nomeCompleto);   
