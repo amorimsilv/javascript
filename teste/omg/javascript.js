71f904da690286055b0154086e5a25028cd9778d
amorim <amorimff35@gmail.com>
2024-06-29 14:05:40 -0300

    1

--- teste/omg/javascript.js
+++ teste/omg/javascript.js
@@ -18,7 +18,8 @@ txt2 = txt2.replace("Hello", "Olá");
 
 console.log(txt2)
 
-// Script Abaixo = a let txt3 esta recebendo "Hello Mundão"
+// Script Abaixo = a let txt3 esta recebendo "Hello Mundão", o if causa o seguinte evento, SE, o txt3 foi IGUAL a "Hello Mundão", o txt3 sera modificado, e a modificação será feita no "Hello", que passará para "Opa",
+// SE NÃO, Se o txt3 não for igual a "Hello Mundão", o txt3 sera modificado, e a modificação será feita também no "Hello", so que agora sera mudado de "Hello" para "Oi";
 
 let txt3 = "Hello Mundão"
 if (txt3 == "Hello Mundão") {
